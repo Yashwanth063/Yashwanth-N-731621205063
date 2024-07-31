73ca1db1b105e68823e609a0e07e16c6509e8e7a
Yash git <yashwanth.n.130304@gmail.com>
2024-07-31 09:01:26 +0530

    angular day2

---
+++ Angular/day2/src/app/app.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+// export class AppComponent {
+//   name : string;
+//   age : number;
+//   email : string;
+//   imageone:string;
+//   fname:string;
+
+
+// constructor(){
+//   this.name = "Yash";
+//   this.age=10;
+//   this.email="yash@gmail.com";
+//   this.imageone="https://miro.medium.com/v2/resize:fit:1358/0*wuNf24urnMp7ypDp.png"
+//   this.fname="";
+
+// }
+// change(){
+//   this.name="vijay";
+// }
+// changeage(){
+//   this.age=21;
+// }
+
+// changeemail(){
+//   this.email="yashmuthu@gmail.com";
+// }
+
+// Image(){
+// this.imageone="https://miro.medium.com/v2/resize:fit:1400/1*bMgQ8MhbnQexpqHgIgBJPA.png"
+
+// }
+
+// }
+
+export class AppComponent{
+  flag : boolean;
+  vech:string[];
+  selectedVechicle:string;
+  mystyle:{};
+style1:string;
+
+  constructor(){
+    this.flag=true;
+    this.vech=["two wheeler","three wheeler","four wheeler"];
+    this.selectedVechicle="";
+    this.mystyle={'width':'200px','border':'2px solid blue','height':'200px'};
+    this.style1="headstyle";
+  }
+
+  changeflag(){
+    this.flag= ! this.flag;
+  }
+
+setSelectedItem(vech : string){
+  this.selectedVechicle=vech;
+}
+changeStyle(){
+  this.mystyle={'width':'300px','border':'2px solid red','height':'200px'};
+}
+}
+
